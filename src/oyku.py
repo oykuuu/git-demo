802bf41c6c426fc35a01d8f878af52de694afe42
oyku.batum@lifemote.com <oyku.batum@lifemote.com>
2024-03-01 16:08:26 +0000

    adding length checks

--- src/oyku.py
+++ src/oyku.py
@@ -1,13 +1,15 @@
 import numpy as np
 
 def addition(array_1, array_2):
-	return array_1 + array_2
+	if len(array_1) != len(array_2):
+		raise ValueError("Array lengths must be the same")
 
-print("hello world")
+	return array_1 + array_2
 
 arr_1 = np.ones([1, 4])
 arr_2 = np.arange(4)
 result = arr_1 + arr_2
 print(result)
 
-print("Oyku is done")
+print("Oyku is done - success!!")
+
