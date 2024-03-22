f20cacf32f8d907442123911a033513bd5402dfb
oyku.batum@lifemote.com <oyku.batum@lifemote.com>
2024-03-22 16:00:07 +0000

    bad sorting example

---
+++ formatting/isort_example.py
@@ -0,0 +1,6 @@
+import numpy as np
+from formatting.black_example import read_data, write_data, function_with_many_arguments, Dataset
+import pandas as pd
+import os
+
+print("The imports will be sorted")
